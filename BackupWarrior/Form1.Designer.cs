bf0af09d27c931c263ec12151d20d6ed04d4a196
Mobile Warrior <mobile.warrior@outlook.com>
2017-09-23 20:00:08 -0500

    form updates

--- BackupWarrior/Form1.Designer.cs
+++ BackupWarrior/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BackupWarrior
 {
-    partial class Form1
+    partial class BackupWarriorForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // Form1
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1175, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 85);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(127, 539);
+            this.panel2.TabIndex = 1;
+            // 
+            // BackupWarriorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 418);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1175, 624);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "BackupWarriorForm";
+            this.Text = "Backup Warrior";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- BackupWarrior/Form1.cs
+++ BackupWarrior/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BackupWarrior
 {
-    public partial class Form1 : Form
+    public partial class BackupWarriorForm : Form
     {
-        public Form1()
+        public BackupWarriorForm()
         {
             InitializeComponent();
         }
